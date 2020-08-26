f7c08c9aaff31de6c4d8893906a43b37e95f168e
micodian <micodian83@gmail.com>
2020-08-26 13:33:34 -0500

    added test function

---
+++ cFunctions/testFunction.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/*
+return_type function_name(parameters){
+    function_body;
+}
+*/
+void display();
+int main(){
+display();
+
+    return 0;
+}
+
+void display(){
+    printf("Function style in C is the same as c++\n");
+}
