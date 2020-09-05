e8caf2cdd8c67094daf3ebef22e9caca5a3f65dc
micodian <micodian83@gmail.com>
2020-09-05 15:26:33 -0500

    testing processes

--- test.c
+++ test.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 
+/*
 int main(){
     int age;
     printf("Enter age\n");
     scanf("%d",&age);
     printf("age is %d",age);
+    return 0;
+}
+*/
+
+int main(){
+    int pid;
+    pid =fork();
+    printf("%d\n",pid);
     return 0;
 }
