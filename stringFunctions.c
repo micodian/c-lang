eb8e5fa9294cb51835c7a9c40013ae1d75abf730
micodian <micodian83@gmail.com>
2020-09-26 16:24:56 -0500

    added strcat examples

--- stringFunctions.c
+++ stringFunctions.c
@@ -11,14 +11,21 @@
 
 int main(int argc, char *argv[]){
 
-char myString[]="Johnson Hathaway Alive!";
+char myString[]=" Johnson Hathaway Alive!";
 char string2[50]; 
+char input[100];
 strcpy(string2,myString);
 
 int stringLength = strlen(myString);
 
 //printf("The length of the string is %d\n",stringLength);
-printf("New string is %s",string2);
+printf("New string is %s\n",string2);
+
+printf("Enter the sentence you want to add\n");
+scanf("%s",input);
+
+printf("The new sentence is %s \n", strcat(input,myString));
+
 
     return 0;
 }
