ebc7a0f8ce62b13105420f2d41978d6e24c6a856
micodian <micodian83@gmail.com>
2020-09-26 16:01:29 -0500

    added strlen example

--- stringFunctions.c
+++ stringFunctions.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+//strlen
+//strcopy
+//strcat
+//strcmp
 
 
 int main(int argc, char *argv[]){
 
+char myString[]="Johnson Hathaway Alive!";
+
+int stringLength = strlen(myString);
+
+printf("The length of the string is %d\n",stringLength);
 
     return 0;
 }
