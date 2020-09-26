3997e3ddfc89b042bceefcab3da0dc6c2099993f
micodian <micodian83@gmail.com>
2020-09-26 16:11:57 -0500

    added strcpy example

--- stringFunctions.c
+++ stringFunctions.c
@@ -12,10 +12,13 @@
 int main(int argc, char *argv[]){
 
 char myString[]="Johnson Hathaway Alive!";
+char string2[50]; 
+strcpy(string2,myString);
 
 int stringLength = strlen(myString);
 
-printf("The length of the string is %d\n",stringLength);
+//printf("The length of the string is %d\n",stringLength);
+printf("New string is %s",string2);
 
     return 0;
 }
